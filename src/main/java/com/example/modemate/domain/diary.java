e84c04eda7399eac66ccbdb60fa686bef515a111
gamgam330 <gamgam0330@naver.com>
2024-07-25 21:13:31 +0900

    update : diary 추가

---
+++ src/main/java/com/example/modemate/domain/diary.java
@@ -0,0 +1,30 @@
+package com.example.modemate.domain;
+
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class diary {
+
+    @Id
+    private Long id;
+
+    private String time;
+
+    private String content;
+
+    private String analyze;
+
+    @ElementCollection
+    private List<String> emotion = new ArrayList<>();
+}
