ea8d8c5821ae076aa45b76f643a5caed21290a56
gamgam330 <gamgam0330@naver.com>
2024-07-25 20:46:13 +0900

    modify : controller API 명세 작성

--- src/main/java/com/example/modemate/Security/custom/CustomUserDetails.java
+++ src/main/java/com/example/modemate/Security/custom/CustomUserDetails.java
@@ -34,7 +34,7 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getUserId().toString();
+        return user.getNickname();
     }
 
     @Override
