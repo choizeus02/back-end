d8e9e4311efd459a3613af7ceea08db72d37963d
choizeus02 <choizeus0212@gmail.com>
2024-08-06 22:30:57 +0900

    [도메인 설정] 도메인 url config 추가

--- src/main/java/com/example/modemate/Config/WebConfig.java
+++ src/main/java/com/example/modemate/Config/WebConfig.java
@@ -15,11 +15,17 @@ public class WebConfig implements WebMvcConfigurer {
     private static final String CORS_URL2 = "http://frontend:5173";
     private static final String CORS_URL3 = "http://3.35.123.191:5173";
 
+    private static final String CORS_URL4 = "http://moodmate.choizeus.com:5173";
+
+    private static final String CORS_URL5 = "http://moodmate.choizeus.com";
+
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping(CORS_URL_PATTERN)
                 .allowedHeaders("*")
-                .allowedOrigins(CORS_URL,CORS_URL2,CORS_URL3)
+                .allowedOrigins(CORS_URL,CORS_URL2,CORS_URL3,CORS_URL4,CORS_URL5)
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
 
